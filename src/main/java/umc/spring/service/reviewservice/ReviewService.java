932e9903d6b3d0110073a78e8c477cec69a5b4de
spenshark <xzx01299@naver.com>
2024-06-12 20:36:32 +0900

    ✨ [FEAT] 가게에 리뷰 추가 API 구현

---
+++ src/main/java/umc/spring/service/reviewservice/ReviewService.java
@@ -0,0 +1,11 @@
+package umc.spring.service.reviewservice;
+
+import umc.spring.domain.Restaurant;
+import umc.spring.domain.Review;
+import umc.spring.web.dto.RestaurantRequestDto;
+import umc.spring.web.dto.ReviewRequestDto;
+
+public interface ReviewService {
+
+    Review createReview(ReviewRequestDto.joinDto request, Long memberId, Long storeId);
+}
